ec77edb38c82470a9743971f48a19235cc3f2eba
lehai1983 <32972247+lehai1983@users.noreply.github.com>
2019-10-12 10:18:46 +0700

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(haname.vn www.haname.vn)
+domains=(dichvudoanhnghiep.online www.dichvudoanhnghiep.online)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="lehai@haname.vn" # Adding a valid address is strongly recommended
